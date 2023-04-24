602d968b3efd7d6ce638a60068b23a8467007212
Subhash Barela <barelasubhash16@gmail.com>
2023-04-24 20:12:39 +0530

    Find the sum of n natural number

--- Program.cs
+++ Program.cs
@@ -10,7 +10,17 @@ namespace Day2PracticeProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Day 2 Practice Problem");
+            Console.WriteLine("Please insert the number to find sum");
+            int num=int.Parse(Console.ReadLine());
+            int temp = num;
+            int sum = 0;
+           while(temp > 0)
+            {
+                sum += temp;
+                temp--;
+            }
+            Console.WriteLine("sum of {0} natural number is {1}",num,sum);
+            Console.ReadLine();
         }
     }
 }

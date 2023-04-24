d9e8fcb55e242538c08328634e6808a132b5d793
Subhash Barela <barelasubhash16@gmail.com>
2023-04-24 19:08:53 +0530

    find table og given number

--- Program.cs
+++ Program.cs
@@ -10,7 +10,29 @@ namespace Day2PracticeProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Day 2 Practice Problem");
+            Console.WriteLine(" Insert the number to find table");
+            int num=int.Parse(Console.ReadLine());
+            int table = 1;
+            for(int i=2; i<num; i++)
+            {
+                Console.Write("table of {0} is: ",i);
+                for(int j=1;j<=10; j++)
+                {
+                    table = i * j;
+                    Console.Write(table+" ");
+                }
+                Console.WriteLine();              
+            }
+            Console.ReadLine();
+
+            //int sqr = 1, i;
+            //Console.WriteLine("insert the number to find sqr");
+            //int num=int.Parse(Console.ReadLine());
+            //for(i=0;i<num; i++)
+            //{
+            //    sqr = sqr * 2;
+            //    Console.WriteLine(sqr);
+            //}
         }
     }
 }

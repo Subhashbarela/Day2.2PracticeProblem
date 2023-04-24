03c29723d3c29ab67977996558e1665d76d2a5f2
Subhash Barela <barelasubhash16@gmail.com>
2023-04-24 17:38:21 +0530

    greeting message of Day 2

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2PracticeProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day 2 Practice Problem");
+        }
+    }
+}

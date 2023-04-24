e97508922f4c86ccf9d0e111510a4004b6b4d029
Subhash Barela <barelasubhash16@gmail.com>
2023-04-24 17:44:38 +0530

    check two number are equal or not

--- Program.cs
+++ Program.cs
@@ -10,7 +10,20 @@ namespace Day2PracticeProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Day 2 Practice Problem");
+            Console.WriteLine("Enter the first number");
+            int num1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if(num1 == num2)
+            {
+                Console.WriteLine("Two number are equal");
+            }
+            else
+            {
+                Console.WriteLine("Two number are not equal");
+                Console.ReadLine();
+            }
         }
     }
 }

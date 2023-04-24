755b5ddf355119b8051130539125908911e18831
Subhash Barela <barelasubhash16@gmail.com>
2023-04-24 19:43:38 +0530

    play flip coin game to Find winner

--- Program.cs
+++ Program.cs
@@ -10,7 +10,31 @@ namespace Day2PracticeProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Day 2 Practice Problem");
+            int Ram = 0;
+            int Sham = 1;
+            Random random= new Random();
+            for(int i=1; i<=20; i++)
+            {
+               int check= random.Next()%2;
+              
+                if(check == 0)
+                {
+                    Ram++;
+                }
+                else
+                {
+                    Sham++;
+                }
+            }
+            if(Ram > Sham)
+            {
+                Console.WriteLine("ram pt is {0} and sham pt {1} so Ram is winner: ",Ram,Sham);
+            }
+            else
+            {
+                Console.WriteLine("sham pt is {0} and ram pt {1} so Sham is winner: " , Sham,Ram);
+
+            }
         }
     }
 }

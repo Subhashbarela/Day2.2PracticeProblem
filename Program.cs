12585df2a5815989960ff7156ac27211c647e9d0
Subhash Barela <barelasubhash16@gmail.com>
2023-04-24 18:34:05 +0530

    Calculate weekend name inserting day number

--- Program.cs
+++ Program.cs
@@ -10,7 +10,52 @@ namespace Day2PracticeProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Day 2 Practice Problem");
+            Console.WriteLine("Please insert the Day");
+            int day=int.Parse(Console.ReadLine());
+            switch (day)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Today is Sunday");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Today is Monday");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Today is Tuesday");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Today is Wednesday");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("Today is Thursday");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("Today is Friday");
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("Today is Saturday");
+                        break;
+                    }
+                    default:
+                    {
+                        Console.WriteLine(" user insert invalid number of day");
+                        break;
+                    }
+            }
+            Console.ReadLine();
         }
     }
 }
